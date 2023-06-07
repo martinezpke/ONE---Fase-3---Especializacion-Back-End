970494353086697feca34de08626f6d869434e6f
Kevin Martínez Palmieri <martinezpke26@gmail.com>
2023-06-06 23:34:19 -0500

    Create CharactersAndString.java

---
+++ Java_primeros_pasos/src/CharactersAndString.java
@@ -0,0 +1,20 @@
+
+public class CharactersAndString {
+	public static void main(String[] args) {
+		// characters 
+		char caracter = 'a';
+		System.out.println(caracter);
+		
+		caracter = 65;
+		System.out.println(caracter);
+		
+		caracter = 65 + 1;
+		char SegundaVaribleChar = (char) (caracter + 1);
+		System.out.println(SegundaVaribleChar);
+		
+		// string
+		
+		String saludo = "Hola, como estas?";
+		System.out.println(saludo);
+	}	
+}
