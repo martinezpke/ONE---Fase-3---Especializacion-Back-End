e179995e0feddbbe3b6ceff23be13a8d9f31bd56
Kevin Martínez Palmieri <martinezpke26@gmail.com>
2023-06-06 23:34:34 -0500

    Create VariableAndMemory.java

---
+++ Java_primeros_pasos/src/VariableAndMemory.java
@@ -0,0 +1,12 @@
+
+public class VariableAndMemory {
+	public static void main(String[] args) {
+		int numero1 = 10;
+		int numero2 = 5;
+		
+		System.out.println(numero2);
+		
+		numero2 = numero1;
+		System.out.println(numero2);
+	}
+}
