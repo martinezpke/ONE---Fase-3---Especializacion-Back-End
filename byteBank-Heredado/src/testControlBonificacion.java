adb390e02e6ca9afc7dfbc37e1325ca203f17fca
Kevin Martínez Palmieri <martinezpke26@gmail.com>
2023-06-11 19:01:32 -0500

    Create testControlBonificacion.java

---
+++ byteBank-Heredado/src/testControlBonificacion.java
@@ -0,0 +1,22 @@
+
+public class testControlBonificacion {
+	public static void main(String[] args) {
+		Funcionario kevin = new Funcionario();
+		kevin.setName("Kevin");
+		kevin.setSalario(1000);
+		
+		Gerente maria = new Gerente();
+		maria.setName("maria");
+		maria.setSalario(10000);
+		 
+		Contador carlos = new Contador();
+		carlos.setName("carlos");
+		carlos.setSalario(5000);
+		
+		 ControlBonificacion controlBonificacion = new ControlBonificacion();
+		 
+		 controlBonificacion.registrarSalario(kevin);
+		 controlBonificacion.registrarSalario(maria);
+		 controlBonificacion.registrarSalario(carlos);
+	}
+}
