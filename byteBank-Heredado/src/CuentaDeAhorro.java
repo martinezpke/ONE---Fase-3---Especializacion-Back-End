e3dedd21bfa7c42e1a12ac5bd563f8a58b2a0e54
Kevin Martínez Palmieri <martinezpke26@gmail.com>
2023-06-14 12:46:21 -0500

    Update CuentaDeAhorro.java

--- byteBank-Heredado/src/CuentaDeAhorro.java
+++ byteBank-Heredado/src/CuentaDeAhorro.java
@@ -4,6 +4,11 @@ public class CuentaDeAhorro extends Cuenta{
 	public CuentaDeAhorro(int aganecia, int numero) {
 		super(aganecia, numero);
 	}
+
+	@Override
+	public void depositar(double saldo) {
+		this.saldo += saldo;
+	}
 	
 	
 	
