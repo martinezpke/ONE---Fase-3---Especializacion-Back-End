f86d20dd59f406ba94c779cedf02052ac0df0a5d
Kevin Martínez Palmieri <martinezpke26@gmail.com>
2023-06-13 19:56:42 -0500

    Create testCuenta.java

---
+++ byteBank-Heredado/src/testCuenta.java
@@ -0,0 +1,14 @@
+
+public class testCuenta {
+	public static void main(String[] args) {
+		// sobre escritura de metodos
+		CuentaCorriente cc = new CuentaCorriente(1, 1);
+		CuentaDeAhorro ca = new CuentaDeAhorro(2, 3);
+		
+		cc.depositar(2000);
+		cc.trasferir(1000, ca);
+		
+		System.out.println("saldo: "+cc.getSaldo());
+		System.out.println("saldo: "+ca.getSaldo());
+	}
+}
