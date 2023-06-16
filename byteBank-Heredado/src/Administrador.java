bf9fa8149b2f9d7cb3d6d806b03d2fa8fd5724dd
Kevin Martínez Palmieri <martinezpke26@gmail.com>
2023-06-16 12:08:49 -0500

    Update Administrador.java

--- byteBank-Heredado/src/Administrador.java
+++ byteBank-Heredado/src/Administrador.java
@@ -1,10 +1,23 @@
 
-public class Administrador extends Funcionario {
-
+public class Administrador extends Funcionario implements Autenticar {
+	
 	@Override
 	public double getBonificacion() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
+	@Override
+	public void setContraseña(String contrasena) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean autenticar(String contrasena) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
 }
